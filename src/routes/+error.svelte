84ef68429b701ceb27a8755fd07ecd0dee55bf52
Klaudia <klaudia.warmus@student.howest.be>
2023-01-24 20:27:40 +0100

    feat: redirect if dog not exists

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+  import NotFound from '$lib//svg/NotFound.svelte'
+</script>
+
+<section class="flex justify-center items-center w-1/2 m-auto">
+  <NotFound />
+</section>
--- src/routes/avatar-creator/edit/[id]/+layout.svelte
+++ src/routes/avatar-creator/edit/[id]/+layout.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
   import type Dog from '$lib/interfaces/dog.interface'
   import { dog } from '$lib/utils/parts'
   import { user } from '$lib/utils/useAuth'
   import { db } from '$lib/utils/useFirebase'
   import { collection, getDocs } from 'firebase/firestore'
-  import { writable } from 'svelte/store'
   import type { PageData } from './$types'
   export let data: PageData
   let myDogs: Dog[] = []
   let foundDog: Dog | undefined
-  console.log(data.props.id)
   if ($user) {
     const dogs = getDocs(collection(db, '3doggy', $user.uid, 'dog'))
     dogs
@@ -18,12 +17,12 @@
       })
       .then(() => {
         foundDog = myDogs.find((dog) => dog.id === data.props.id)
-        dog.set(foundDog)
+        if (foundDog) dog.set(foundDog)
+        else goto('/')
       })
   }
 </script>
 
-{#if foundDog}
+{#if $dog}
   <slot />
-  <!-- TODO: NOT FOUND -->
 {/if}
