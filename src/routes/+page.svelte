46bbf06c2c67ca06ce50ad6d062168420d75ffc6
Klaudia <klaudia.warmus@student.howest.be>
2023-01-25 20:11:48 +0100

    feat: random quotes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,44 @@
 <script lang="ts">
   import { goto } from '$app/navigation'
-  import { user } from '$lib/utils/useAuth'
-  import type { User } from 'firebase/auth'
-  $: console.log(user)
-  let userValue: User | null = null
-  user.subscribe((value) => {
-    userValue = value
-  })
+  import type Quote from '$lib/interfaces/quote.interface'
+  import { db } from '$lib/utils/useFirebase'
+  import { collection, getDocs } from 'firebase/firestore'
+  import { onMount } from 'svelte'
+
+  let randomQuote: Quote
+
+  const quotes: Quote[] = [
+    {
+      quote: 'Dogs are not our whole life, but they make our lives whole.',
+      author: 'Roger Caras',
+    },
+    {
+      quote: 'The better I get to know men, the more I find myself loving dogs.',
+      author: 'Charles de Gaulle',
+    },
+    {
+      quote: 'A dog is the only thing on earth that loves you more than he loves himself.',
+      author: 'Josh Billings',
+    },
+    {
+      quote:
+        'The greatest fear dogs know is the fear that you will not come back when you go out the door without them.',
+      author: 'Stanley Coren',
+    },
+    {
+      quote: 'Happiness is a warm puppy.',
+      author: 'Charles Schulz',
+    },
+    {
+      quote: 'Everyone thinks they have the best dog, and none of them are wrong.',
+      author: 'W.R. Purche',
+    },
+  ]
+
+  const getRandomQuote = () => {
+    randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+  }
+  getRandomQuote()
 </script>
 
 <svelte:head>
@@ -19,7 +51,7 @@
 >
   <div class="flex flex-col gap-6 max-w-sm items-center sm:items-start">
     <h3 class="italic text-2xl font-cormorant font-medium">
-      "Everyone thinks they have the best dog, and none of them are wrong." - Unknown
+      "{randomQuote.quote}" - {randomQuote.author}
     </h3>
     <h2 class="text-xl">Make your life better and create your canine friend in 3D!</h2>
     <button
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,5 @@
 import type { PageLoad } from './$types'
 import { user } from '$lib/utils/useAuth'
-
-export const load: PageLoad = async ({ params }) => {
+export const load: PageLoad = async () => {
   await user.known
 }
